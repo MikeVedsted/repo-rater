8528c1d7a7d50bb656cb0a43e793c9280b88aed8
Mike Vedsted <mikevedsted@gmail.com>
2021-03-01 11:52:09 +0200

    Add SignUp form

--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Button from '../Button';
+import FormikTextInput from '../FormikTextInput';
 
-const SignUpForm = () => {
+import theme from '../../theme';
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: theme.colors.white, padding: 20 },
+});
+
+const SignUpForm = ({ onSubmit }) => {
   return (
-    <View>
-      <Text>FORM HERE</Text>
+    <View style={styles.container}>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+      <FormikTextInput name="passwordConfirm" placeholder="Confirm password" secureTextEntry />
+      <Button onPress={onSubmit}>Sign up</Button>
     </View>
   );
 };
