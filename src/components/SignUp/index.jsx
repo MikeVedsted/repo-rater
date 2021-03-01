98098b65dc9e2df38d8c65ebfda987d0295e756a
Mike Vedsted <mikevedsted@gmail.com>
2021-03-01 11:58:13 +0200

    Add SignUp validation

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { StyleSheet } from 'react-native';
+import { useHistory } from 'react-router-native';
+
+import theme from '../../theme';
+
+import SignUpForm from './SignUpForm';
+
+const initialValues = {
+  username: "",
+  password: "",
+  passwordConfirm: ""
+};
+
+
+const validationSchema = yup.object().shape({
+  username: yup.string().min(1, 'Username must be at least 1 character').max(30, 'Maximum length of username is 30 characters').required('Username is required'),
+  password: yup.string().min(5, 'Password must be at least 5 character').max(50, 'Maximum length of password is 50 characters').required('Password is required'),
+  passwordConfirm: yup.string()
+    .oneOf([yup.ref('password'), null], 'Passwords do not match')
+    .required('Password confirm is required')
+});
 
 const SignUp = () => {
+  const history = useHistory();
+
+  const onSubmit = async (values) => {
+    console.log(values);
+    // const { username, password } = values;
+
+    // try {
+    //   const { data } = await signIn({ username, password });
+    //   console.log(data);
+    //   history.push('/');
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  };
+
   return (
-    <View>
-      <Text>Sign up here</Text>
-    </View>
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema} >
+      {({ handleSubmit }) => <SignUpForm onSubmit={handleSubmit} />}
+    </Formik>
   );
 };
 
