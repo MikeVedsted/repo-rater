63710a03168779dce29717a3a3976768e63cd3df
Mike Vedsted <mikevedsted@gmail.com>
2021-03-01 12:42:14 +0200

    Add exercise 10.22

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Formik } from 'formik';
 import * as yup from 'yup';
+import { Formik } from 'formik';
+import { useMutation } from '@apollo/client';
 import { useHistory } from 'react-router-native';
 
-import useSignIn from '../../hooks/useSignIn';
-import { useMutation, useApolloClient } from '@apollo/client';
-import { SIGN_UP } from '../../graphql/mutations';
-
-import theme from '../../theme';
-
 import SignUpForm from './SignUpForm';
+import useSignIn from '../../hooks/useSignIn';
+import { SIGN_UP } from '../../graphql/mutations';
 
 const initialValues = {
   username: "",
@@ -17,10 +14,15 @@ const initialValues = {
   passwordConfirm: ""
 };
 
-
 const validationSchema = yup.object().shape({
-  username: yup.string().min(1, 'Username must be at least 1 character').max(30, 'Maximum length of username is 30 characters').required('Username is required'),
-  password: yup.string().min(5, 'Password must be at least 5 character').max(50, 'Maximum length of password is 50 characters').required('Password is required'),
+  username: yup.string()
+    .min(1, 'Username must be at least 1 character')
+    .max(30, 'Maximum length of username is 30 characters')
+    .required('Username is required'),
+  password: yup.string()
+    .min(5, 'Password must be at least 5 character')
+    .max(50, 'Maximum length of password is 50 characters')
+    .required('Password is required'),
   passwordConfirm: yup.string()
     .oneOf([yup.ref('password'), null], 'Passwords do not match')
     .required('Password confirm is required')
@@ -32,19 +34,11 @@ const SignUp = () => {
   const [mutate] = useMutation(SIGN_UP);
 
   const onSubmit = async (values) => {
-    console.log(values);
     const { username, password } = values;
-
     try {
-      const { data } = await mutate({ variables: { username, password } });
-      console.log(data);
-      const signinres = await signIn({ username, password });
-      console.log('sign in', signinres.data);
+      await mutate({ variables: { username, password } });
+      await signIn({ username, password });
       history.push('/');
-
-    //   const { data } = await signIn({ username, password });
-    //   console.log(data);
-    //   history.push('/');
     } catch (e) {
       console.log(e);
     }
