a0a3bd7a5d75f999a3d7259f819bfae38a653bd9
Mike Vedsted <mikevedsted@gmail.com>
2021-03-03 14:46:26 +0200

    Add MyReviews view

--- src/components/MyReviews/index.jsx
+++ src/components/MyReviews/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { useQuery } from '@apollo/react-hooks';
+
+import ReviewItem from '../ReviewItem';
+import { CHECK_AUTH } from '../../graphql/queries';
+
+const dummy = [{ node: { id: 'aasdasd', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }, { node: { id: 'aasdasdasd26', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }, { node: { id: 'aasd2sda964815sd', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }, { node: { id: 'a861sdasd', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }];
 
 const MyReviews = () => {
+  const { data, error, loading, refetch } = useQuery(CHECK_AUTH, { variables: { includeReviews: true } });
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    if (!loading && data) {
+      console.log(data.authorizedUser.reviews.edges);
+      setReviews(data.authorizedUser.reviews.edges);
+    }
+  }, [data]);
+
+  if (loading) {
+    return (
+      <View><Text>Loading...</Text></View>
+    );
+  }
+
   return (
-    <View><Text>My reviews here</Text></View>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        // data={repository.reviews.edges}
+        data={reviews}
+        // onEndReached={onEndReach}
+        // onEndReachedThreshold={0.5}
+        renderItem={({ item }) => <ReviewItem review={item} />}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+        keyExtractor={({ node }) => node.id}
+      // ListHeaderComponent={() => <RepositoryInfo data={repository} />}
+      />
+    </View>
   );
 };
 
