a3a5f2caa26779a8d3507068b09fc0077ef20b62
Mike Vedsted <mikevedsted@gmail.com>
2021-03-03 14:53:48 +0200

    Add exercise 10.26

--- src/components/MyReviews/index.jsx
+++ src/components/MyReviews/index.jsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
+import { View, Text, FlatList } from 'react-native';
 
 import ReviewItem from '../ReviewItem';
 import { CHECK_AUTH } from '../../graphql/queries';
 
-const dummy = [{ node: { id: 'aasdasd', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }, { node: { id: 'aasdasdasd26', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }, { node: { id: 'aasd2sda964815sd', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }, { node: { id: 'a861sdasd', createdAt: 123, user: { username: 'mike' }, rating: 10, text: 'asdasdasdasdasdasdasdasd' } }];
-
 const MyReviews = () => {
-  const { data, error, loading, refetch } = useQuery(CHECK_AUTH, { variables: { includeReviews: true } });
+  const { data, loading } = useQuery(CHECK_AUTH, { variables: { includeReviews: true } });
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     if (!loading && data) {
-      console.log(data.authorizedUser.reviews.edges);
       setReviews(data.authorizedUser.reviews.edges);
     }
   }, [data]);
@@ -27,14 +24,10 @@ const MyReviews = () => {
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-        // data={repository.reviews.edges}
         data={reviews}
-        // onEndReached={onEndReach}
-        // onEndReachedThreshold={0.5}
         renderItem={({ item }) => <ReviewItem review={item} />}
         ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
         keyExtractor={({ node }) => node.id}
-      // ListHeaderComponent={() => <RepositoryInfo data={repository} />}
       />
     </View>
   );
